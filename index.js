1af5ea736479c572fb54a07437a15b359e77d921
Sahil Aujla <sahilaujla15@gmail.com>
2022-07-26 20:35:36 -0400

    Initial commit

---
+++ index.js
@@ -0,0 +1,27 @@
+const { ethers, utils } =   require('ethers');
+require("dotenv").config({ path: ".env" });
+var url = process.env.QUICKNODE_WSS_URL;
+
+var init = function () {
+  var customWsProvider = new ethers.providers.WebSocketProvider(url);
+  
+  customWsProvider.on("pending", (tx) => {
+    customWsProvider.getTransaction(tx).then(function (transaction) {
+      console.log(transaction);
+    });
+  });
+
+  customWsProvider._websocket.on("error", async () => {
+    console.log(`Unable to connect to ${ep.subdomain} retrying in 3s...`);
+    setTimeout(init, 3000);
+  });
+  customWsProvider._websocket.on("close", async (code) => {
+    console.log(
+      `Connection lost with code ${code}! Attempting reconnect in 3s...`
+    );
+    customWsProvider._websocket.terminate();
+    setTimeout(init, 3000);
+  });
+};
+
+init();
---
+++ readme.md
@@ -0,0 +1,8 @@
+# Watching mempool
+
+Script to watch the mempool constantly.
+
+## Use cases
+
+1. Can be used to make frontrunning bots.
+2. Can be used to keep track of transactions for a particular address or contract.
