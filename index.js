9fb4b76ef9220c6877451a355fa2710167129368
Sahil Aujla <sahilaujla15@gmail.com>
2022-07-31 11:13:15 -0400

    Added the code to listen for receive transactions for a particular address

--- index.js
+++ index.js
@@ -1,13 +1,30 @@
 const { ethers, utils } =   require('ethers');
 require("dotenv").config({ path: ".env" });
 var url = process.env.QUICKNODE_WSS_URL;
+var ALCHEMY_API_KEY = process.env.ALCHEMY_API_KEY;
+
+var provider = new ethers.providers.AlchemyProvider("rinkeby", ALCHEMY_API_KEY);
 
 var init = function () {
   var customWsProvider = new ethers.providers.WebSocketProvider(url);
-  
+
   customWsProvider.on("pending", (tx) => {
     customWsProvider.getTransaction(tx).then(function (transaction) {
-      console.log(transaction);
+      if (transaction.to === "0x0B53E89cFD388f54A3683AAfc5974db4593B6641" && Number(utils.formatEther(transaction.value)) > 0) {
+        console.log(`0x0B53E89cFD388f54A3683AAfc5974db4593B6641 will receive ${utils.formatEther(transaction.value)} ether: Transaction pending 🚨`);
+        const txnCheck = async (txnHash) => {
+          let txn_receipt = await provider.getTransaction(txnHash);
+          if (txn_receipt) {
+            if (txn_receipt.blockNumber) {
+              // console.log(`txn done: ${txn_receipt}`);
+              console.log(`0x0B53E89cFD388f54A3683AAfc5974db4593B6641 received ${utils.formatEther(txn_receipt.value)} ether ✅`);
+              clearInterval(interval);
+              return txn_receipt
+            }
+          }
+        }
+        let interval = setInterval(async() => await txnCheck(transaction.hash), 5000);
+      }
     });
   });
 
